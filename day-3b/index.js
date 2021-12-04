df7f7f48aadc220ac1a5aa59f1710fe7e2ca92c5
Taylor Andrews <88749086+bossanova456@users.noreply.github.com>
2021-12-04 13:01:55 -0500

    Finished 3b

--- day-3b/index.js
+++ day-3b/index.js
@@ -14,6 +14,24 @@ const req = https.get(options, res => {
 
     res.on('data', d => {
         const inputData = d.toString('utf8').split('\n');
+
+        const binaryLength = inputData[0].length;
+        let oxygen = inputData, co2 = inputData;
+        const reducer = (previous, current) => (previous ? parseInt(previous) : 0) + (current ? parseInt(current) : 0);
+
+        for (let i=0; i<binaryLength && oxygen.length>1; i++) {
+            const onesCount = oxygen.map(value => value[i]).reduce(reducer);
+            oxygen = oxygen.filter(binaryValue => binaryValue[i] === (onesCount >= oxygen.length / 2.0 ? "1" : "0"));
+        }
+
+        for (let i=0; i<binaryLength && co2.length>1; i++) {
+            const onesCount = co2.map(value => value[i]).reduce(reducer);
+            co2 = co2.filter(binaryValue => binaryValue[i] === (onesCount >= co2.length / 2.0 ? "0" : "1"));
+        }
+
+        const lifeSupport = parseInt(oxygen[0], 2) * parseInt(co2[0], 2);
+
+        console.log("Oxygen: " + oxygen + " | CO2: " + co2 + " | Life Support: " + lifeSupport);
     })
 })
 
