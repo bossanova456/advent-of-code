2577aa634e5ceb22776edcea4cc6c0220ea63400
Taylor Andrews <88749086+bossanova456@users.noreply.github.com>
2021-12-10 20:52:26 -0500

    Finished solution for 6b

--- day-6b/index.js
+++ day-6b/index.js
@@ -16,66 +16,40 @@ const req = https.get(options, res => {
         const inputData = d.toString('utf8').split(',');
 
         console.log("Input data: " + inputData);
-        // let fishArray = inputData.map(fish => parseInt(fish));
-        let fishArray = [3, 4, 3, 1, 2];
+        let fishArray = inputData.map(fish => parseInt(fish));
 
-        // newFishCount tracks the number of new fish with the same day count
-        // newFishDays tracks the number of days for each set of newFish with the same day count
-        let newFishCount = Array();
-        let newFishDays = Array();
-        const days = 18;
+        // We could consolidate all the initial inputs and set up the counts accordingly,
+        // but for simplicity we just handle each one individually until we start adding fish
+        const fishCounts = Array(fishArray.length).fill(1);
+        const days = 256;
 
         console.log("Initial state: " + fishArray);
+        console.log("Fish counts: " + fishCounts);
+        console.log("Days: " + days);
 
         for (let i=0; i<days; i++) {
-            const newFishCountLength = newFishCount.length;
-            const newFishCountCopy = [...newFishCount];
-            const newFishDaysCopy = [...newFishDays];
+            const fishLength = fishArray.length;
 
-            for (let j=0; j<newFishCountLength; j++) {
-                // First check the new fish arrays
-                if (newFishDaysCopy[j] > 0) {
-                    newFishDaysCopy[j]--;
+            for (let j=0; j<fishLength; j++) {
+                if (fishArray[j] > 0) {
+                    fishArray[j]--;
                 }
                 else {
-                    newFishDaysCopy[j] = 6;
-                    newFishCountCopy.push(1);
-                    newFishDaysCopy.push(8);
-                }
-            }
-
-            newFishCount = [...newFishCountCopy];
-            newFishDays = [...newFishDaysCopy];
-
-            let fishCopy = [...fishArray];
-            for (let j=0; j<fishCopy.length; j++) {
-
-                // Next, check the initial array, and append to new fish arrays if necessary
-                // Doing this second prevents new fish from having day decremented before required
-                if (fishCopy[j] > 0) {
-                    fishCopy[j]--;
-                }
-                else {
-                    console.log("Spawning new fish --- New fish count length: " + newFishCountLength + " | Days: " + newFishDays[newFishCountLength-1] + " | Count: " + newFishCount[newFishCountLength-1]);
-                    fishCopy[j] = 6;
-                    if (newFishCountLength >= 0 && newFishDays[newFishCountLength] === 8) {
-                        newFishCount[newFishCountLength]++;
+                    fishArray[j] = 6;
+                    if (fishArray.length > fishLength && fishArray[fishLength] === 8) {
+                        fishCounts[fishLength] += fishCounts[j];
                     }
                     else {
-                        newFishCount.push(1);
-                        newFishDays.push(8);
+                        fishArray.push(8);
+                        fishCounts.push(fishCounts[j]);
                     }
                 }
             }
-
-            // fishArray = [...fishCopy, ...newFish];
-            fishArray = [...fishCopy];
-            console.log("After day " + i + ": " + fishArray + " | " + newFishDays + " | " + newFishCount);
         }
 
-        // console.log("Number of fish: " + fishArray.length);
-        const numberOfFish = fishArray.length + newFishCount.reduce((previous, current) => previous + current);
-        console.log("Number of fish: " + numberOfFish);
+        console.log("Fish array: " + fishArray);
+        console.log("Fish counts: " + fishCounts);
+        console.log("Number of fish: " + fishCounts.reduce((previous, current) => previous + current));
     })
 })
 
